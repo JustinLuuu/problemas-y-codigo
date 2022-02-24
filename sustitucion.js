7b7a784d1e7a52fd62b244268f95aa21771f8461
Justin Jr <77954884+JustinLuuu@users.noreply.github.com>
2022-02-24 11:34:31 -0400

    Update sustitucion.js

--- sustitucion.js
+++ sustitucion.js
@@ -4,20 +4,16 @@
 function reemplazar(oracion, palabraElim, palabraAgr){
   const arrayOracion = oracion.split(" ");
   const indicePalabraElim = arrayOracion.indexOf(palabraElim);
-
-  return indicePalabraElim >=0 ? 
-  arrayOracion.reduce((ac, palabra, indiceElemento)=> {
-    ac+= ` ${(indiceElemento === indicePalabraElim) ? palabraAgr : palabra}`;
-
-    return ac;
-  }, '').trim() 
-  :
-  `La palabra '${palabraElim}' no se encuentra en '${oracion}'`
+  if(indicePalabraElim >= 0) {
+    arrayOracion[indicePalabraElim] = palabraAgr;
+    return arrayOracion.join(" ")
+  } else {
+    return `La palabra '${palabraElim}' no se encuentra en '${oracion}'`
+  }
 }
 
 
 const resultado =
-reemplazar('Pepe pela papas con un pico', 'Pepe', 'Justin' );
+reemplazar('Pepe pela papas con un pico', 'pela', 'Justin' );
 
 console.log(resultado);
-
